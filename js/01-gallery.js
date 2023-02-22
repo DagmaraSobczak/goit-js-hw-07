3da1edb59d0f14ae516ad4d530b9cf15e31b6834
DagmaraSobczak <daga.sobczak@gmail.com>
2023-02-22 22:26:49 +0100

    gallery-item

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+import * as basicLightbox from "basiclightbox";
+const gallery = document.querySelector(".gallery");
+
+function createGalleryItem(item) {
+  const galleryItem = document.createElement("div");
+  galleryItem.classList.add("gallery__item");
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = item.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = item.preview;
+  galleryImage.dataset.source = item.original;
+  galleryImage.alt = item.description;
+
+  galleryLink.appendChild(galleryImage);
+  galleryItem.appendChild(galleryLink);
+
+  return galleryItem;
+}
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const galleryItem = createGalleryItem(galleryItems[i]);
+  gallery.appendChild(galleryItem);
+}
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("gallery__image")) {
+    const source = event.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${source}">`);
+    instance.show();
+  }
+});
